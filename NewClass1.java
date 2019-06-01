ec87b9abb060413429a1854af4bb3a86e2b81e0d
MyskUnion <50514582+MyskUnion@users.noreply.github.com>
2019-06-01 17:49:47 +0300

    Add files via upload

---
+++ NewClass1.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Пекарня
+ */
+public class NewClass1 {
+
+    private static String row2;
+    public static void main (String[] args){
+        
+        String[][] demoArray = new String [3][3];
+        
+        String[] row1 = ("X","","O")
+        demoArray [0]= row1;
+        ()      String[] /*name*/ = String[];
+row2 = ("O","O","X")
+        demoArray[2] = row2;
+        String[] row3 = ("","","O")
+        demoArray[3] = row3;
+        
+           for (String[] demoArray1 : demoArray) {
+        }
+        int i = 0;
+for (int j = 0; j < demoArray [i] [j]]
+        int j = 0;
+        System.out.printf ("%2s",demoArray [i][j]);
+    }
+    System.out.printin();
+            }
